c66c6baccb9c92a773e09c2850fa6f56430baf28
savaw <savaw97@gmail.com>
2019-08-09 18:18:25 +0430

    add html css

--- twitter/views.py
+++ twitter/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
+from .models import Tweet
+from django.utils import timezone
 # Create your views here.
 
 
 def tweet_list(request):
-    return render(request, 'twitter/tweet_list.html', {})
+    tweets = Tweet.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'twitter/tweet_list.html', {'tweets': tweets})
